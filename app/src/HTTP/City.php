4781a2bb32f79cba9e891815211a50b8a3702886
Evgeniy Kuvshinov <smpl@itmywork.com>
2019-12-06 16:42:00 +0500

    example middleware for check city custom implements call

--- app/src/HTTP/City.php
+++ app/src/HTTP/City.php
@@ -25,11 +25,10 @@ class City implements RequestHandlerInterface
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $city = $request->getAttribute('city');
-        if (!in_array($city, $this->cities)) {
-            return new JsonResponse(['City not Found'], 404, ['Access-Control-Allow-Origin' => '*']);
-        }
-        $weather = $this->wheather->getWeather($city);
-        return new JsonResponse($weather, 200, ['Access-Control-Allow-Origin' => '*']);
+        return new JsonResponse(
+            $this->wheather->getWeather($request->getAttribute('city')),
+            200,
+            ['Access-Control-Allow-Origin' => '*']
+        );
     }
 }
--- app/src/Matcher.php
+++ app/src/Matcher.php
@@ -3,10 +3,13 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\HTTP\Accu;
 use App\HTTP\CitiesAvailable;
 use App\HTTP\City;
+use App\HTTP\CityMustBeAvailble;
 use App\HTTP\Hello;
 use App\HTTP\NotFound;
+use App\HTTP\OpenWheatherMap;
 use Cekta\Routing\Nikic\DispatcherBuilder;
 use Cekta\Routing\Nikic\Handler;
 use Cekta\Routing\Nikic\ProviderHandler;
@@ -20,8 +23,10 @@ class Matcher extends \Cekta\Routing\Nikic\Matcher
     ) {
         $builder = new DispatcherBuilder();
         $builder->get('/', Hello::class);
-        $builder->get('/city/{city:\w+}', City::class);
+        $builder->get('/city/{city:\w+}', City::class, CityMustBeAvailble::class);
         $builder->get('/cities', CitiesAvailable::class);
+        $builder->get('/accu/{city:\w+}', Accu::class, CityMustBeAvailble::class);
+        $builder->get('/open/{city:\w+}', OpenWheatherMap::class, CityMustBeAvailble::class);
         parent::__construct(
             new Handler(NotFound::class),
             $builder->build(),
